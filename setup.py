6c6e81b372778d642aabc8f0e0f872f5dd438826
Andrew Bauer <abau171@gmail.com>
2016-07-07 18:22:23 -0400

    added license and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name = "highfive",
+    packages = ["highfive"],
+    version = "0.0",
+    description = "Failure-tolerant, cooperative distributed processing framework.",
+    author = "Andrew Bauer",
+    author_email = "abau171@gmail.com",
+    url = "https://github.com/abau171/highfive",
+    #download_url = "",
+    keywords = ["distributed", "queue", "task queue", "job queue", "collaborative"],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Internet",
+        "Topic :: Software Development",
+        "Topic :: System :: Distributed Computing",
+    ],
+)
+
