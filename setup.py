f17431b527169c0d9377e75abfadfd6dee1fde6b
Andrew Bauer <abau171@gmail.com>
2016-07-08 11:23:57 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 from setuptools import setup
 
+long_desc = """
+HighFive is a distributed processing framework designed to be used for
+collaborative computation. It is made to be fault-tolerant to network issues
+and manual disconnects so workers can come and go as they please without
+disrupting running processes.
+
+"""
+
 setup(
     name = "highfive",
     packages = ["highfive"],
     version = "0.0",
-    description = "Fault-tolerant, cooperative distributed processing framework.",
+    description =
+        "Fault-tolerant, cooperative distributed processing framework.",
+    long_description = long_desc,
     author = "Andrew Bauer",
     author_email = "abau171@gmail.com",
     url = "https://github.com/abau171/highfive",
     #download_url = "",
-    keywords = ["distributed", "queue", "task queue", "job queue", "collaborative"],
+    license = "MIT",
+    keywords =
+        ["distributed", "queue", "task queue", "job queue", "collaborative"],
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
