e7e4144ecd956b8e337e3b51f0c3ff2fb6bc1b6d
Andrew Bauer <abau171@gmail.com>
2016-09-23 18:54:17 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup
 
 long_desc = """
-HighFive is a simple distributed processing framework designed to support a large pool of transient workers coming and going as they please. Workers running in the pool are invisible to the owner, so there is no need to worry about distributing tasks manually or the possibility of network issues as these are handled by the framework. HighFive is made for cooperative processing of CPU-heavy tasks by allowing you and your friends to chip in as much computing power as you want, for as long as you want!
+HighFive is a simple distributed processing framework designed so you and your friends can donate CPU time to each other to run large, interesting computations. It's easy to spin up a HighFive master and connect as many remote workers as you want to it!
 
-HighFive is written in Python 3, and uses asyncio for managing connections. Because it uses async/await syntax, it is currently only supported by Python 3.5 and up (this may change in the future).
+Don't want to lock down all your computers for hours while you wait for results? Since HighFive was designed for cooperative work, the workers can
+disconnect at any time without harming the main process! Then, they can reconnect to the same process later and get back to work!
 
+HighFive is written in Python 3, and uses asyncio for managing connections. Because it uses async/await syntax, it is currently only supported by Python 3.5 and up.
 """
 
 setup(
     name = "highfive",
     packages = ["highfive"],
-    version = "0.1",
+    version = "0.2",
     description =
         "Framework for fault-tolerant cooperative distributed processing.",
     long_description = long_desc,
     author = "Andrew Bauer",
     author_email = "abau171@gmail.com",
     url = "https://github.com/abau171/highfive",
-    download_url = "https://github.com/abau171/highfive/archive/v0.1.tar.gz",
+    download_url = "https://github.com/abau171/highfive/archive/v0.2.tar.gz",
     license = "MIT",
     keywords =
         ["distributed", "queue", "task queue", "job queue", "collaborative"],
